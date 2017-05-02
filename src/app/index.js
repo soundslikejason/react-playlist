ea4db3f4ff05c751e83811e2acd7f6afe55e2f69
Jason Leong <soundslikejason@users.noreply.github.com>
2017-05-02 10:58:23 -0700

    cycling through data

--- src/app/index.js
+++ src/app/index.js
@@ -5,25 +5,22 @@ var ReactDOM = require('react-dom');
 var TodoComponent = React.createClass({
   getInitialState: function() {
     return {
-      todos: ['wash up', 'eat some cheese', 'take a nap'],
+      todos: ['wash up', 'eat some cheese', 'take a nap', 'buy flowers'],
       age: 30
     }
   }, // getInitialState
   render: function() {
-    var ager = setTimeout(function() {
-      this.setState({
-        age: 35
-      });
-    }.bind(this), 5000);
+    var todos = this.state.todos;
+    todos = todos.map(function(item, index) {
+      return(
+        <li>{item}</li>
+      )
+    });
     return(
       <div id="todo-list">
         <p>The busiest people have the most leisure...</p>
         <p>{this.state.age}</p>
-        <ul>
-          <li>{this.state.todos[0]}</li>
-          <li>{this.state.todos[1]}</li>
-          <li>{this.state.todos[2]}</li>
-        </ul>
+        <ul>{todos}</ul>
       </div>
     );
   } // render

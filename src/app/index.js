5cda1f844d154a340e580fdee42e16305270d8ae
Jason Leong <soundslikejason@users.noreply.github.com>
2017-05-02 12:33:22 -0700

    nesting components

--- src/app/index.js
+++ src/app/index.js
@@ -6,24 +6,35 @@ var TodoComponent = React.createClass({
   getInitialState: function() {
     return {
       todos: ['wash up', 'eat some cheese', 'take a nap', 'buy flowers'],
-      age: 30
     }
   }, // getInitialState
   render: function() {
     var todos = this.state.todos;
     todos = todos.map(function(item, index) {
       return(
-        <li>{item}</li>
+        <TodoItem item={item} key={index} />
       )
     });
     return(
       <div id="todo-list">
         <p>The busiest people have the most leisure...</p>
-        <p>{this.state.age}</p>
         <ul>{todos}</ul>
       </div>
     );
   } // render
+});
+
+// Create TodoItem component
+var TodoItem = React.createClass({
+  render: function() {
+    return(
+      <li>
+        <div className="todo-item">
+          <span className="item-name">{this.props.item}</span>
+        </div>
+      </li>
+    );
+  }
 });
 
 // put component into html page

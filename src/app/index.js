b27d85b28acc5446a7b5549c292ddc8ca92f9a56
Jason Leong <soundslikejason@users.noreply.github.com>
2017-05-02 13:15:19 -0700

    handling events

--- src/app/index.js
+++ src/app/index.js
@@ -12,16 +12,26 @@ var TodoComponent = React.createClass({
     var todos = this.state.todos;
     todos = todos.map(function(item, index) {
       return(
-        <TodoItem item={item} key={index} />
+        <TodoItem item={item} key={index} onDelete={this.onDelete} />
       )
-    });
+    }.bind(this));
     return(
       <div id="todo-list">
         <p>The busiest people have the most leisure...</p>
         <ul>{todos}</ul>
       </div>
     );
-  } // render
+  }, // render
+  
+  // Custom functions
+  onDelete: function(item) {
+    var updatedTodos = this.state.todos.filter(function(val, index) {
+      return item !== val;
+    });
+    this.setState({
+      todos: updatedTodos
+    });
+  }
 });
 
 // Create TodoItem component
@@ -31,9 +41,15 @@ var TodoItem = React.createClass({
       <li>
         <div className="todo-item">
           <span className="item-name">{this.props.item}</span>
+          <span className="item-delete" onClick={this.handleDelete}> x </span>
         </div>
       </li>
     );
+  },
+  
+  // Custom functions
+  handleDelete: function() {
+    this.props.onDelete(this.props.item);
   }
 });
 
